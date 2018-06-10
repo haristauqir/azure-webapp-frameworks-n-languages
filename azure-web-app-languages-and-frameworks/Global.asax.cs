2bb97f7f9eb7268939fac116d0a62721f668329e
haristauqir <haristauqir@gmail.com>
2018-06-10 19:15:25 +0100

    project creation
    project creation

---
+++ azure-web-app-languages-and-frameworks/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace azure_web_app_languages_and_frameworks
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
---
+++ azure-web-app-languages-and-frameworks/Node.js
@@ -0,0 +1,12 @@
+﻿var http = require('http');
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello this is a Node.js)');
+
+}).listen(process.env.PORT);
+
+
+var pjson = require('./package.json');
+console.log(pjson.version); //** log version
